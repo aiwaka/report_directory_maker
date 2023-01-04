fcb579576df9e5e44ffd574794a8b33e7eb2efdb
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-01-04 18:03:13 +0900

    config parser feature

--- create_report_dir/cli.py
+++ create_report_dir/cli.py
@@ -1,15 +1,35 @@
 import os
 import sys
-from typing import Union
+from typing import Union, Dict
 
 import fire
+import yaml
 
 from .utils import detect_invalid_char, error_print
 from .temp import latex_template
 
 
-def get_config():
+def get_config() -> Dict[str, Union[None, str]]:
     """コンフィグデータを取得する"""
+    # 存在確認
+    homedir = os.path.expanduser("~")
+    config_path = os.path.join(homedir, ".texdirmakerrc")
+    if not os.path.isfile(config_path):
+        # 存在しない場合作成
+        print("config file not found. create it?[y/N] -> ", end="")
+        if input() == "y":
+            with open(config_path, mode="w") as f:
+                default_settings = {"preamble": None, "author": "author"}
+                yaml.dump(default_settings, f)
+            print(f"File '{config_path}' created. Edit it later")
+            return default_settings
+        else:
+            print("canceled")
+            sys.exit(0)
+    else:
+        with open(config_path) as f:
+            config = yaml.safe_load(f)
+            return config
 
 
 def create_dir(dir_name: str, force: bool) -> str:
@@ -18,7 +38,7 @@ def create_dir(dir_name: str, force: bool) -> str:
     if detect_invalid_char(dir_name):
         error_print(f"The directory name '{dir_name}' is not available.")
         sys.exit(1)
-    new_full_path = os.path.normpath(cwd + "/" + dir_name)
+    new_full_path = os.path.normpath(os.path.join(cwd, dir_name))
 
     try:
         os.mkdir(new_full_path)
@@ -42,6 +62,10 @@ def app(
     dir_name, filename: Union[str, None] = None, force: bool = False, title: str = ""
 ):
     """アプリ本体"""
+    config = get_config()
+    preamble_path = config.get("preamble")
+    author = str(config.get("author"))
+
     dir_name = str(dir_name)
     if filename is None:
         filename = dir_name + ".tex"
@@ -52,8 +76,8 @@ def app(
         filename += ".tex"
 
     new_full_path = create_dir(dir_name, force)
-    filepath = new_full_path + "/" + filename
-    create_file(filepath, title, "author")
+    filepath = os.path.join(new_full_path, filename)
+    create_file(filepath, title, author)
 
 
 def main():
